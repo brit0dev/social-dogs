cbcdb2dd0a713352b40d539dce104347f7f99840
brito <britoifpe@gmail.com>
2024-09-05 16:51:21 -0300

    Finishing the Header.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import styles from './Header.module.css';
 import { NavLink } from 'react-router-dom';
+import DogsIcon from '../Assets/dogs.svg?react';
 
 function Header() {
   return (
     <header className={styles.header}>
-      <nav className='container'>
-        <NavLink to='/' end>
-          Home
+      <nav className={styles.nav + ' container'}>
+        <NavLink to='/' end aria-label='Dogs - Home'>
+          <DogsIcon className={styles.logo} />
         </NavLink>
-        <NavLink to='/login'>Login / Criar</NavLink>
+        <NavLink to='/login' className={styles.login}>
+          Login / Criar
+        </NavLink>
       </nav>
     </header>
   );
