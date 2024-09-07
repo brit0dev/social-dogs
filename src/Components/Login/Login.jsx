09ca948478880746fdcaa5b951177af213082a1e
brito <britoifpe@gmail.com>
2024-09-07 07:39:46 -0300

    Login page and form.

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
+import { UserContext } from '../../UserContext';
+import styles from './Login.module.css';
 
 function Login() {
+  const { login } = React.useContext(UserContext);
+  const navigate = useNavigate();
+  React.useEffect(() => {
+    if (login) navigate('/conta');
+  }, [login, navigate]);
+
   return (
-    <section>
-      <Routes>
-        <Route path='/' element={<LoginForm />} />
-        <Route path='create' element={<LoginCreate />} />
-        <Route path='recoverpassword' element={<LoginPasswordLost />} />
-        <Route path='resetpassword' element={<LoginPasswordLost />} />
-      </Routes>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path='/' element={<LoginForm />} />
+          <Route path='create' element={<LoginCreate />} />
+          <Route path='recoverpassword' element={<LoginPasswordLost />} />
+          <Route path='resetpassword' element={<LoginPasswordLost />} />
+        </Routes>
+      </div>
     </section>
   );
 }
--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
+import { UserContext } from '../../UserContext';
+import Error from '../Helper/Error';
+import styles from './LoginForm.module.css';
+import stylesBtn from '../Forms/Button.module.css';
+import { Link } from 'react-router-dom';
 
 function LoginForm() {
+  const { userLogin, loading, error } = React.useContext(UserContext);
+
+  const username = useForm();
+  const password = useForm();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    userLogin(username.value, password.value);
+  }
+
   return (
-    <section>
-      <h1>Login</h1>
+    <section className={styles.container + ' animeLeft'}>
+      <h1 className='title'>Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <Input label='Usuário' name='username' {...username} />
+        <Input label='Senha' type='password' name='password' {...password} />
+
+        {loading ? (
+          <Button disabled>Carregando</Button>
+        ) : (
+          <Button>Entrar</Button>
+        )}
+        {error && <Error message={error} />}
+      </form>
+      <Link className={styles.recoverPass} to='/login/recoverpassword'>
+        Esqueci minha senha.
+      </Link>
+      <div className={styles.signIn}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link className={stylesBtn.button} to='/login/create'>
+          Cadastro
+        </Link>
+      </div>
     </section>
   );
 }
