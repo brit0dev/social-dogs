c3f527ca2ffda8354bfa92573aa5bb0cf8af84c3
brito <britoifpe@gmail.com>
2024-09-07 11:55:57 -0300

    Creating User component base.

---
+++ src/Components/User/UserHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserHeaderNav from './UserHeaderNav';
+
+function UserHeader() {
+  return (
+    <header>
+      <h1 className='title'>Titulo</h1>
+      <UserHeaderNav />
+    </header>
+  );
+}
+
+export default UserHeader;
---
+++ src/Components/User/UserHeaderNav.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function UserHeaderNav() {
+  return <nav></nav>;
+}
+
+export default UserHeaderNav;
