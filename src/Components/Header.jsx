5cd57e795c113c0a0a6c9b2cb3c6795dd3f9b907
brito <britoifpe@gmail.com>
2024-09-07 11:49:14 -0300

    Adding user avatar on header when logged.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,17 +2,28 @@ import React from 'react';
 import styles from './Header.module.css';
 import { NavLink } from 'react-router-dom';
 import DogsIcon from '../Assets/dogs.svg?react';
+import { UserContext } from '../UserContext';
 
 function Header() {
+  const { data } = React.useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={styles.nav + ' container'}>
         <NavLink to='/' end aria-label='Dogs - Home'>
           <DogsIcon className={styles.logo} />
         </NavLink>
-        <NavLink to='/login' className={styles.login}>
-          Login / Criar
-        </NavLink>
+        {data ? (
+          <div>
+            <NavLink to='/conta' className={styles.login}>
+              {data.username}
+            </NavLink>
+          </div>
+        ) : (
+          <NavLink to='/login' className={styles.login}>
+            Login / Criar
+          </NavLink>
+        )}
       </nav>
     </header>
   );
