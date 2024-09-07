229a8a9e736c3f9c9c7b838cb3d3ad70a84f8831
brito <britoifpe@gmail.com>
2024-09-07 12:05:05 -0300

    Adding userHeader, userHeaderNav and useMedia Hook.

--- src/Components/User/UserHeader.jsx
+++ src/Components/User/UserHeader.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import styles from './UserHeader.module.css';
 import UserHeaderNav from './UserHeaderNav';
+import { useLocation } from 'react-router-dom';
 
 function UserHeader() {
+  const [title, setTitle] = React.useState('');
+  const location = useLocation();
+
+  React.useEffect(() => {
+    switch (location.pathname) {
+      case '/conta/estatisticas':
+        setTitle('Estatísticas');
+        break;
+      case '/conta/postar':
+        setTitle('Poste Sua Foto');
+        break;
+      default:
+        setTitle('Minha conta');
+
+        break;
+    }
+  }, [location]);
+
   return (
-    <header>
-      <h1 className='title'>Titulo</h1>
+    <header className={styles.header}>
+      <h1 className='title'>{title}</h1>
       <UserHeaderNav />
     </header>
   );
--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,7 +1,61 @@
 import React from 'react';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
+import styles from './UserHeaderNav.module.css';
+import FeedIcon from '../../Assets/feed.svg?react';
+import StatsIcon from '../../Assets/estatisticas.svg?react';
+import AddIcon from '../../Assets/adicionar.svg?react';
+import LogoutIcon from '../../Assets/sair.svg?react';
+import useMedia from '../../Hooks/useMedia';
 
 function UserHeaderNav() {
-  return <nav></nav>;
+  const { userLogout } = React.useContext(UserContext);
+  const [mobileMenu, setMobileMenu] = React.useState();
+  const mobile = useMedia('(max-width:40rem)');
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
+
+  function handleLogout() {
+    navigate('/login');
+    userLogout();
+  }
+
+  return (
+    <>
+      {mobile && (
+        <button
+          aria-label='Menu'
+          className={`${styles.mobileButton} ${
+            mobileMenu && styles.mobileButtonActive
+          }`}
+          onClick={() => setMobileMenu(!mobileMenu)}
+        ></button>
+      )}
+      <nav
+        className={`${mobile ? styles.navMobile : styles.nav} ${
+          mobileMenu && styles.navMobileActive
+        }`}
+      >
+        <NavLink to='/conta' end>
+          <FeedIcon /> {mobile && 'Minhas fotos'}
+        </NavLink>
+        <NavLink to='/conta/estatisticas'>
+          <StatsIcon /> {mobile && 'Estatísticas'}
+        </NavLink>
+        <NavLink to='/conta/postar'>
+          <AddIcon /> {mobile && 'Adicionar Fotos'}
+        </NavLink>
+        <button onClick={handleLogout}>
+          <LogoutIcon />
+          {mobile && 'Sair'}
+        </button>
+      </nav>
+    </>
+  );
 }
 
 export default UserHeaderNav;
