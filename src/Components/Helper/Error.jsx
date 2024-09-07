3141492396270097465562cbaccba0a6740dbf23
brito <britoifpe@gmail.com>
2024-09-07 07:32:59 -0300

    Creating error component for forms.

---
+++ src/Components/Helper/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Error.module.css';
+
+function Error({ message }) {
+  if (!message) return null;
+  return <p className={styles.error}>{message}</p>;
+}
+
+export default Error;
