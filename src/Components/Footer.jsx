85b39fe7f8f2571fb2f23d80aaed01b23aeb066d
brito <britoifpe@gmail.com>
2024-09-11 05:25:37 -0300

    Adding footer component.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import DogsIcon from '../Assets/dogs-footer.svg?react';
 
 function Footer() {
-  return <footer className={styles.footer}>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <DogsIcon />
+      <p>Social Dogs. Alguns direitos reservados.</p>
+    </footer>
+  );
 }
 
 export default Footer;
