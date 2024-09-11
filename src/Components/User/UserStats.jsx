a57356fa9bd350f8d3e1823b8a875286792b5e48
brito <britoifpe@gmail.com>
2024-09-11 12:54:34 -0300

    Add statistics page using victory charts.

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import Head from '../Helper/Head';
+import useFetch from '../../Hooks/useFetch';
+import { STATS_GET } from '../../api';
+import UserStatsGraphs from './UserStatsGraphs';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
 
 function UserStats() {
-  return (
-    <div>
-      <Head title='Estatísticas' />
-      Estatisticas
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData(event) {
+      const { url, options } = STATS_GET();
+
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  console.log(data);
+
+  if (loading) return <Loading />;
+  if (error) return <Error message={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title='Estatísticas' />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 }
 
 export default UserStats;
